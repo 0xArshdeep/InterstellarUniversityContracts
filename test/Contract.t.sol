0d306b9ca2b14081235d013133a18d51300df0b9
Arshdeep Ballagan <arshsb1998@gmail.com>
2022-06-07 17:25:22 -0700

    Initial contract commits.

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "../src/Contract.sol";
 import "forge-std/Test.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+    Contract public myContract;
+
+    function setUp() public {
+        myContract = new Contract();
+    }
 
     function testExample() public {
         assertTrue(true);
